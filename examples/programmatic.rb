9960267025a2208e6d921c74fcbdcde329b40a40
Jacques Fuentes <jpfuentes2@gmail.com>
2013-03-02 17:06:57 -0600

    Add vi/emacs editing mode to programmatic example

--- examples/programmatic.rb
+++ examples/programmatic.rb
@@ -1,8 +1,20 @@
 require "ender"
 
+modes = [:vi, :emacs] # vi > emacs
+
+begin
+  mode = modes.shift
+  Readline.public_send "#{mode}_editing_mode?"
+rescue NotImplementedError
+  retry unless modes.empty?
+  mode = nil
+end
+
 Ender.configure do
   # define our servers here
   server host: "127.0.0.1", group: :lo
+
+  edit_mode mode if mode
 end
 
 # Readline prompt will start & block so that nothing after this line is executed.
